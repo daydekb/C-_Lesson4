5372de0090b968d0f1ad5ce25cfed36712485883
Dmitriu Ydin <da-ekb@mail.ru>
2024-01-23 11:24:50 +0500

    add Sem_4

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 1
+Задайте одномерный массив, заполненный случайными
+числами. Определите количество простых чисел в этом
+массиве.
+Примеры
+[1 3 4 19 3] => 2
+[4 3 4 1 9 5 21 13] => 3
+*/
+
+int[] CreateRandomArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+int SimpleNumCounter(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        
+    }
+    return count;
+}
+
+array
